7ebaf240032e9dfd4b95812f2b3095742e41520d
EliasEduardoBarbosa <eliaseduardobarbosa@gmail.com>
2022-12-15 15:51:59 -0300

    CLI and setup.py updated

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name='acppred',
     version='0.0.1',
     packages=find_packages(),
-    author='Elias Eduardo Barbosa da Rosa'
+    author='Elias Eduardo Barbosa da Rosa',
+    entry_points = {
+        'console_script': [
+            'acppred-train = acppred.train:main',
+        ]
+    }
 )
